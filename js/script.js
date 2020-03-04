c024d17ee6e1cee48ef5facea4fdcc6f449eed49
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-03-04 22:57:19 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function getMoveName(argMoveId) {
+const getMoveName = function(argMoveId) {
 if(argMoveId === 1){
     return 'kamień';
     }else if(argMoveId === 2) {
@@ -11,7 +11,7 @@ printMessage('Nie znam ruchu o id ' + argMoveId + '.');
 return 'nieznany ruch';
 }
 
-function displayResult(argComputerMove, argPlayerMove) {
+const displayResult = function(argComputerMove, argPlayerMove) {
     if(argComputerMove === argPlayerMove) {
         printMessage('Remis!');
     }else if(argComputerMove === 'kamień' && argPlayerMove === 'papier') {
@@ -27,20 +27,28 @@ function displayResult(argComputerMove, argPlayerMove) {
     }
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+const playGame = function(playerInput) {
+    clearMessages();
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = getMoveName(randomNumber);
+    console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Mój ruch to: ' + computerMove);
+    let computerMove = getMoveName(randomNumber);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    printMessage('Mój ruch to: ' + computerMove);
 
-console.log('Gracz wpisał: ' + playerInput);
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let playerMove = getMoveName(playerInput);
+    console.log('Gracz wpisał: ' + playerInput);
 
-printMessage('Twój ruch to: ' + playerMove);
+    let playerMove = getMoveName(playerInput);
 
-displayResult(computerMove, playerMove);
+    printMessage('Twój ruch to: ' + playerMove);
+
+    displayResult(computerMove, playerMove);
+}
+
+document.getElementById('play-rock').addEventListener("click", function() {playGame(1);} );
+document.getElementById('play-paper').addEventListener("click", function() {playGame(2);} );
+document.getElementById('play-scissors').addEventListener("click", function() {playGame(3);} );
